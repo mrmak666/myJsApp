00bdc055df9ebe9a06afb9baa3b128c5f5613127
mrmak666 <mrmak666@gmail.com>
2019-06-10 02:06:03 +0400

    db json update

--- public/Scripts/Script.js
+++ public/Scripts/Script.js
@@ -3,37 +3,44 @@
 /// <reference path="angular-route.js" />
 var myApp = angular.module("myModule", ["ngRoute"])
     .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.caseInsensitiveMatch = true;
+
         $routeProvider.when("/home", { templateUrl: "../Templates/home.html", controller: "homeController" })
             .when("/courses", { templateUrl: "../Templates/courses.html", controller: "coursesController" })
             .when("/students", { templateUrl: "../Templates/students.html", controller: "studentsController" })
             .when("/students/:id", { templateUrl: "../Templates/studentDetails.html", controller: "studentDetailsController as studentDetailsCtrl" }) // we can use controller as syntax
-            .otherwise({redirectTo : "/home"})  
-            $locationProvider.html5Mode(true);    
+            .otherwise({ redirectTo: "/home" })
+        $locationProvider.html5Mode(true);
     })
     .controller("homeController", function ($scope) {
-        
+
         $scope.message = "Home Page";
     })
     .controller("coursesController", function ($scope) {
         $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server", "AngularJS", "JavaScript"];
     })
-     .controller("studentsController", function ($scope, $http) {
-         $http.get("https://my-json-server.typicode.com/mrmak666/myJsApp/employees")
-                                .then(function (response) {
-                                    $scope.students = response.data;
-                                })
-                                
-                                
-     })
-     .controller("studentDetailsController", function ($http, $routeParams,$log) {
-        var vm=this;
+    .controller("studentsController", function ($scope, $http, $route) {
+        
+        // to reload list when click on the students link, it will refresh the controller only
+        var vm = this;
+        vm.reloadData = function () { $route.reload(); }
+        //-------------------------------------------------------------------------------
+        $http.get("https://my-json-server.typicode.com/mrmak666/myJsApp/employees")
+            .then(function (response) {
+                $scope.students = response.data;
+            })
+
+
+    })
+    .controller("studentDetailsController", function ($http, $routeParams, $log) {
+        var vm = this;
         $http({
             url: "https://my-json-server.typicode.com/mrmak666/myJsApp/employees/",
             method: "get",
             params: { id: $routeParams.id }
-        }).then(function (response,err) {
-           if(err) throw err;
-           $log.info(response.data[0]);           
-           vm.student = response.data[0];
+        }).then(function (response, err) {
+            if (err) throw err;
+            $log.info(response.data[0]);
+            vm.student = response.data[0];
         })
     });
