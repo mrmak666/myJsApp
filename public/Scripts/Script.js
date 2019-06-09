fdf8419a4923aebcfd72aeb853bd98117c0d10d4
mrmak666 <mrmak666@gmail.com>
2019-06-09 23:50:23 +0400

    move script to public, fix hash in angular route URL

--- public/Scripts/Script.js
+++ public/Scripts/Script.js
@@ -2,11 +2,12 @@
 /// <reference path="angular.js" />
 /// <reference path="angular-route.js" />
 var myApp = angular.module("myModule", ["ngRoute"])
-    .config(function ($routeProvider) {
-        $routeProvider.when("/home", { templateUrl: "Templates/home.html", controller: "homeController" })
-            .when("/courses", { templateUrl: "Templates/courses.html", controller: "coursesController" })
-            .when("/students", { templateUrl: "Templates/students.html", controller: "studentsController" })
-            .otherwise({redirectTo : "./home"})            
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when("/home", { templateUrl: "../Templates/home.html", controller: "homeController" })
+            .when("/courses", { templateUrl: "../Templates/courses.html", controller: "coursesController" })
+            .when("/students", { templateUrl: "../Templates/students.html", controller: "studentsController" })
+            .otherwise({redirectTo : "/home"})  
+            $locationProvider.html5Mode(true);    
     })
     .controller("homeController", function ($scope) {
         
