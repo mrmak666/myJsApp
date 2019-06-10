f7b8f2bfda9aaea8d932ab208659bf6d4cc7b8c3
mrmak666 <mrmak666@gmail.com>
2019-06-10 17:30:32 +0400

    update json.db with student Totals

--- public/Scripts/Script.js
+++ public/Scripts/Script.js
@@ -2,13 +2,22 @@
 /// <reference path="angular.js" />
 /// <reference path="angular-route.js" />
 var myApp = angular.module("myModule", ["ngRoute"])
+
     .config(function ($routeProvider, $locationProvider) {
         $routeProvider.caseInsensitiveMatch = true;
 
         $routeProvider.when("/home", { templateUrl: "../Templates/home.html", controller: "homeController" })
             .when("/courses", { templateUrl: "../Templates/courses.html", controller: "coursesController" })
-            .when("/students", { templateUrl: "../Templates/students.html", controller: "studentsController as studentsCtrl" })
-            .when("/students/:id", { templateUrl: "../Templates/studentDetails.html", controller: "studentDetailsController as studentDetailsCtrl" }) // we can use controller as syntax
+            .when("/students", { templateUrl: "../Templates/students.html", controller: "studentsController as studentsCtrl" ,
+            resolve: {
+                studentslist: function ($http) {
+                    return $http.get("https://my-json-server.typicode.com/mrmak666/myJsApp/employees/")
+                            .then(function (response) {
+                                return response.data;
+                            })
+                }} 
+        })
+            .when("/students/:id", { templateUrl: "../Templates/studentDetails.html", controller: "studentDetailsController", controllerAs: "studentDetailsCtrl"}) // we can use controller as syntax
             .otherwise({ redirectTo: "/home" })
         $locationProvider.html5Mode(true);
     })
@@ -19,16 +28,42 @@ var myApp = angular.module("myModule", ["ngRoute"])
     .controller("coursesController", function ($scope) {
         $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server", "AngularJS", "JavaScript"];
     })
-    .controller("studentsController", function ($http, $route) {
+    .controller("studentsController", function (studentslist, $route,$scope,$rootScope, $log  ) {
         
-        // to reload list when click on the students link, it will refresh the controller only
+        // confirm leave page:
+        $scope.$on("$routeChangeStart", function (event, next, current) {
+            if (!confirm("Are you sure you want to navigate away from this page to " + next.$$route.originalPath)) {
+                event.preventDefault();
+            }
+        });
+        // or using location change where next and current will have the full URL info:
+        // $scope.$on("$locationChangeStart", function (event, next, current) {
+        //     if (!confirm("Are you sure you want to navigate away from this page to " + next)) {
+        //         event.preventDefault();
+        //     }
+        // });
+        // --------------------------------------------
+        $rootScope.$on("$locationChangeStart", function () {
+            $log.debug("$locationChangeStart fired");
+        });
+    
+        $rootScope.$on("$routeChangeStart", function () {
+            $log.debug("$routeChangeStart fired");
+        });
+    
+        $rootScope.$on("$locationChangeSuccess", function () {
+            $log.debug("$locationChangeSuccess fired");
+        });
+    
+        $rootScope.$on("$routeChangeSuccess", function () {
+            $log.debug("$routeChangeSuccess fired");
+        });
+
+        // to reload list , it will refresh the controller only
         var vm = this;
         vm.reloadData = function () { $route.reload(); }
         //-------------------------------------------------------------------------------
-        $http.get("https://my-json-server.typicode.com/mrmak666/myJsApp/employees")
-            .then(function (response) {
-                vm.students = response.data;
-            })
+        vm.students = studentslist;     
 
 
     })
